65599ff314091d673108a47ded8dcbd59d74791b
hamzaja <hamzajaved71@gmail.com>
2019-11-02 16:46:54 -0400

    notesss

---
+++ app.js
@@ -0,0 +1,10 @@
+// const fs = require('fs')
+// fs.writeFileSync('notes.txt', 'new file created by node js!!')
+// fs.appendFileSync('notees.txt', "adding more stuff")
+ const a = 'abc'
+
+ const getNotes = function getNotes() {
+   return "your notes..."
+ }
+
+module.exports = getNotes
---
+++ notes.js
@@ -0,0 +1,2 @@
+const getNotes = require('./app.js')
+console.log(getNotes())
